1082d70a7884f829e235e048040e7b325f49467b
no92 <no92.mail@gmail.com>
2023-06-14 22:52:57 +0200

    pci.h: define PCI configuration space constants

--- src/pci.h
+++ src/pci.h
@@ -2,6 +2,10 @@
 
 #include <stdint.h>
 
+#define PCI_HDR_VENDOR_ID 0x00
+#define PCI_HDR_PROG_IF 0x09
+#define PCI_HDR_SUBCLASS 0x0A
+
 #define PCI_ASLS 0xFC
 
 void lil_get_bar(void* device, int num, uintptr_t* base, uintptr_t* len);
