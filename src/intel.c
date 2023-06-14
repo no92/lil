4e7a62cb0c87d9eb74aa6667639dba009515adbd
no92 <no92.mail@gmail.com>
2023-06-14 23:02:50 +0200

    init: check PCI (sub)class

--- src/intel.c
+++ src/intel.c
@@ -1,18 +1,24 @@
 #include <lil/intel.h>
-#include "ivy_bridge/ivb.h"
-#include "coffee_lake/cfl.h"
-
 #include <lil/imports.h>
 
+#include "src/coffee_lake/cfl.h"
+#include "src/ivy_bridge/ivb.h"
+#include "src/pci.h"
+
 void lil_init_gpu(LilGpu* ret, void* device) {
-   uint32_t config_0 = lil_pci_readd(device, 0);
+   uint32_t config_0 = lil_pci_readd(device, PCI_HDR_VENDOR_ID);
     if (config_0 == 0xffffffff) {
         return;
     }
-    uint16_t device_id = (uint16_t)(config_0 >> 16);
-    uint16_t vendor_id = (uint16_t)config_0;
 
+    uint16_t device_id = (uint16_t) (config_0 >> 16);
+    uint16_t vendor_id = (uint16_t) config_0;
     if (vendor_id != 0x8086) {
+        return;
+    }
+
+    uint32_t class = lil_pci_readw(device, PCI_HDR_SUBCLASS);
+    if (class != 0x300) {
         return;
     }
 
