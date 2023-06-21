e63ceea9a4a26dec840a05a53721a87de966036c
no92 <no92.mail@gmail.com>
2023-06-21 09:41:48 +0200

    intel: add LilEncoder struct for eDP

--- include/lil/intel.h
+++ include/lil/intel.h
@@ -87,6 +87,52 @@ typedef struct LilCrtc {
     void (*commit_modeset) (struct LilGpu* gpu, struct LilCrtc* crtc);
 } LilCrtc;
 
+typedef struct LilEncoderEdp {
+	uint8_t backlight_control_method_type;
+	uint8_t backlight_inverter_type;
+	uint8_t backlight_inverter_polarity;
+	uint16_t pwm_inv_freq;
+	uint32_t initial_brightness;
+	uint16_t pwm_on_to_backlight_enable;
+
+	uint8_t edp_vswing_preemph;
+	bool edp_iboost;
+	bool edp_port_reversal;
+	bool ssc_bits;
+	bool edp_downspread;
+	bool edp_fast_link_training;
+	bool edp_fast_link_training_supported;
+	uint8_t edp_fast_link_rate;
+	uint8_t edp_fast_link_lanes;
+	uint8_t edp_max_link_rate;
+	uint8_t edp_max_lanes;
+	uint8_t edp_lane_count;
+	uint8_t edp_dpcd_rev;
+	int edp_color_depth;
+	uint8_t edp_balance_leg_val;
+
+	bool edp_vbios_hotplug_support;
+	bool edp_full_link_params_provided;
+
+	uint32_t t1_t3;
+	uint32_t t8;
+	uint32_t t9;
+	uint32_t t10;
+	uint32_t t11_12;
+
+	bool t3_optimization;
+	bool dynamic_cdclk_supported;
+
+	uint16_t supported_link_rates[8];
+	size_t supported_link_rates_len;
+} LilEncoderEdp;
+
+typedef struct LilEncoder {
+	union {
+		LilEncoderEdp edp;
+	};
+} LilEncoder;
+
 typedef struct LilConnectorInfo {
     uint32_t num_modes;
     LilModeInfo* modes;
@@ -106,6 +152,18 @@ typedef struct PllLilLimits {
     LilLimit p2;
 } PllLilLimits;
 
+enum LilAuxChannel {
+	AUX_CH_A,
+	AUX_CH_B,
+	AUX_CH_C,
+	AUX_CH_D,
+	AUX_CH_E,
+	AUX_CH_F,
+	AUX_CH_G,
+	AUX_CH_H,
+	AUX_CH_I,
+};
+
 typedef struct LilConnector {
     bool (*is_connected) (struct LilGpu* gpu, struct LilConnector* connector);
     LilConnectorInfo (*get_connector_info) (struct LilGpu* gpu, struct LilConnector* connector);
@@ -118,6 +176,7 @@ typedef struct LilConnector {
     PllLilLimits limits;
 
     LilCrtc* crtc;
+	LilEncoder *encoder;
 
     bool on_pch;
 
