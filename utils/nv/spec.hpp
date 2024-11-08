2d694c3ea197a4796003b6cd738fe53e78ea89b6
no92 <leo@managarm.org>
2024-11-08 01:03:59 +0100

    util/nv: initial VBIOS parsing

--- utils/nv/spec.hpp
+++ utils/nv/spec.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
-#include <stdint.h>
+#include <array>
+#include <cstdint>
 
 namespace pci {
+
+constexpr uint32_t IFR_SIGNATURE = 0x4947564E;
+constexpr uint32_t RFRD_SIGNATURE = 0x44524652;
 
 struct [[gnu::packed]] ifr_header {
 	uint32_t signature;
@@ -22,13 +26,13 @@ struct [[gnu::packed]] rfrd_header {
 namespace rom {
 
 struct [[gnu::packed]] header {
-	char signature[2];
+	std::array<uint8_t, 2> signature;
 	uint8_t reserved[0x16];
 	uint16_t pcir_offset;
 };
 
 struct [[gnu::packed]] efi_header {
-	char signature[2];
+	std::array<uint8_t, 2> signature;
 	// in units of 512 bytes
 	uint16_t initialization_size;
 	uint32_t efi_signature;
@@ -42,7 +46,17 @@ struct [[gnu::packed]] efi_header {
 
 static_assert(sizeof(header) == sizeof(efi_header));
 
+constexpr uint32_t PCIR_SIGNATURE = 0x52494350;
+constexpr uint32_t NPDS_SIGNATURE = 0x5344504E;
+constexpr uint32_t RGIS_SIGNATURE = 0x53494752;
+
 struct [[gnu::packed]] pcir {
+	enum code_type : uint8_t {
+		PC_AT = 0x00,
+		EFI = 0x03,
+		NV_EXTENDED_VBIOS = 0xE0,
+	};
+
 	uint32_t signature;
 	uint16_t vendor;
 	uint16_t device;
@@ -52,12 +66,14 @@ struct [[gnu::packed]] pcir {
 	uint8_t class_code[3];
 	uint16_t image_length;
 	uint16_t revision_level;
-	uint8_t code_type;
+	code_type code_type;
 	uint8_t indicator;
 	uint16_t reserved1;
 };
 
 static_assert(sizeof(pcir) == 0x18);
+
+constexpr uint32_t NPDE_SIGNATURE = 0x4544504E;
 
 struct [[gnu::packed]] npde {
 	uint32_t signature;
@@ -68,9 +84,9 @@ struct [[gnu::packed]] npde {
 	uint8_t flags;
 };
 
-}
+} // namespace rom
 
-}
+} // namespace pci
 
 namespace bit {
 
@@ -119,6 +135,6 @@ struct [[gnu::packed]] desc_header {
 
 static_assert(sizeof(desc_header) == 4);
 
-}
+} // namespace falcon
 
-}
+} // namespace bit
